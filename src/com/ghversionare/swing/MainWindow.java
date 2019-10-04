622750bc33f0d1f881a10312d8e2368e8b7cb4e1
anyikit <anyikit@TIMW0155.wamas.com>
2019-10-04 14:09:47 +0300

    clean up

--- src/com/ghversionare/swing/MainWindow.java
+++ src/com/ghversionare/swing/MainWindow.java
@@ -26,31 +26,25 @@ public class MainWindow {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
 	public MainWindow() {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 1000, 900);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(0, 143, 145, 139);
-		panel_1.setBackground(new Color(0, 0, 255));
+		JPanel panel1 = new JPanel();
+		panel1.setBounds(0, 143, 145, 139);
+		panel1.setBackground(new Color(0, 0, 255));
 		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBounds(0, 280, 145, 145);
-		panel_3.setBackground(new Color(0, 51, 255));
+		JPanel panel3 = new JPanel();
+		panel3.setBounds(0, 280, 145, 145);
+		panel3.setBackground(new Color(0, 51, 255));
 		frame.getContentPane().setLayout(null);
-		frame.getContentPane().add(panel_1);
-		frame.getContentPane().add(panel_3);
+		frame.getContentPane().add(panel1);
+		frame.getContentPane().add(panel3);
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(51, 0, 255));
@@ -60,19 +54,19 @@ public class MainWindow {
 		JLabel lblHome = new JLabel("Home");
 		panel.add(lblHome);
 		
-		JPanel panel_4 = new JPanel();
-		panel_4.setBackground(new Color(0, 102, 255));
-		panel_4.setBounds(0, 423, 145, 145);
-		frame.getContentPane().add(panel_4);
+		JPanel panel4 = new JPanel();
+		panel4.setBackground(new Color(0, 102, 255));
+		panel4.setBounds(0, 423, 145, 145);
+		frame.getContentPane().add(panel4);
 		
-		JPanel panel_5 = new JPanel();
-		panel_5.setBackground(new Color(0, 204, 255));
-		panel_5.setBounds(0, 566, 145, 145);
-		frame.getContentPane().add(panel_5);
+		JPanel panel5 = new JPanel();
+		panel5.setBackground(new Color(0, 204, 255));
+		panel5.setBounds(0, 566, 145, 145);
+		frame.getContentPane().add(panel5);
 		
-		JPanel panel_6 = new JPanel();
-		panel_6.setBackground(new Color(0, 255, 255));
-		panel_6.setBounds(0, 707, 145, 154);
-		frame.getContentPane().add(panel_6);
+		JPanel panel6 = new JPanel();
+		panel6.setBackground(new Color(0, 255, 255));
+		panel6.setBounds(0, 707, 145, 154);
+		frame.getContentPane().add(panel6);
 	}
 }
