b655d2f6bb838009a1fb1712ccb04159d380f7cd
adriannyikita <adriannyikita@192.168.0.157>
2019-11-13 21:10:04 +0200

    last update

--- src/com/ghversionare/swing/LogIn.java
+++ src/com/ghversionare/swing/LogIn.java
@@ -22,6 +22,7 @@ import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import org.kohsuke.github.GHAuthorization;
 import org.kohsuke.github.GitHub;
 import javax.swing.JPasswordField;
 
@@ -83,24 +84,23 @@ public class LogIn {
 		JLabel lblPassword = new JLabel("Password");
 
 		JButton btnNewButton = new JButton("Log IN!");
-
+		GitHub a=null;
+		try {
+			a = GitHub.connectAnonymously();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		try {
+			GHAuthorization gh = a.createToken(null, null, null);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
 		btnNewButton.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
-				try {
-					String user = usernameField.getText();
-					String pass = passwordField.getText();
-					GitHub gh = GitHub.connectUsingPassword(user, pass);
-					if (!gh.isCredentialValid()) {
-						JOptionPane.showMessageDialog(null, "Credentials are wrong! please try again");
-					} else if (!gh.isAnonymous()) {
-						openMain(gh);
-					} else {
-						JOptionPane.showMessageDialog(null, "Login Failed! Please try again!");
-					}
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
 			}
 		});
 
@@ -127,56 +127,36 @@ public class LogIn {
 				openMain(gh);
 			}
 		});
-		
+
 		passwordField = new JPasswordField();
 
 		GroupLayout groupLayout = new GroupLayout(getFrame().getContentPane());
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.TRAILING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.TRAILING).addGroup(groupLayout
+				.createSequentialGroup()
+				.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-							.addGroup(groupLayout.createSequentialGroup()
-								.addGap(217)
-								.addComponent(lblNewLabel))
-							.addGroup(groupLayout.createSequentialGroup()
-								.addGap(194)
-								.addComponent(lblPassword))
-							.addGroup(groupLayout.createSequentialGroup()
-								.addGap(174)
-								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-									.addComponent(passwordField, 130, 130, Short.MAX_VALUE)
-									.addComponent(usernameField)))
-							.addGroup(groupLayout.createSequentialGroup()
-								.addGap(174)
-								.addComponent(btnAnonymous, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(182)
-							.addComponent(btnNewButton))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(195)
-							.addComponent(lblUsername)))
-					.addContainerGap(148, Short.MAX_VALUE))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(29)
-					.addComponent(lblNewLabel)
-					.addGap(41)
-					.addComponent(lblUsername)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(usernameField, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
-					.addGap(11)
-					.addComponent(lblPassword)
-					.addGap(5)
-					.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(12)
-					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(btnAnonymous)
-					.addContainerGap(157, Short.MAX_VALUE))
-		);
+								.addGroup(groupLayout.createSequentialGroup().addGap(217).addComponent(lblNewLabel))
+								.addGroup(groupLayout.createSequentialGroup().addGap(194).addComponent(lblPassword))
+								.addGroup(groupLayout.createSequentialGroup().addGap(174)
+										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+												.addComponent(passwordField, 130, 130, Short.MAX_VALUE)
+												.addComponent(usernameField)))
+								.addGroup(groupLayout.createSequentialGroup().addGap(174).addComponent(btnAnonymous,
+										GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+						.addGroup(groupLayout.createSequentialGroup().addGap(182).addComponent(btnNewButton))
+						.addGroup(groupLayout.createSequentialGroup().addGap(195).addComponent(lblUsername)))
+				.addContainerGap(148, Short.MAX_VALUE)));
+		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup().addGap(29).addComponent(lblNewLabel).addGap(41)
+						.addComponent(lblUsername).addPreferredGap(ComponentPlacement.RELATED)
+						.addComponent(usernameField, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+						.addGap(11).addComponent(lblPassword).addGap(5)
+						.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE)
+						.addGap(12)
+						.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED).addComponent(btnAnonymous)
+						.addContainerGap(157, Short.MAX_VALUE)));
 		getFrame().getContentPane().setLayout(groupLayout);
 		getFrame().setBounds(100, 100, 452, 419);
 		getFrame().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
